7d450b91368e9b87bbfdd3e2dcc0bc25a1ccf38e
chaewon <juh7814@gmail.com>
2023-07-20 15:32:51 +0900

    💄 타이틀 폰트 적용

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,12 +3,14 @@ import { useRouter } from "next/router";
 import cls from "../lib/cls";
 
 
+
 export default function Nav() {
     const router = useRouter()
 
     return (
         <div className="absolute top-0 p-4" >
-            <h1 className={cls("text-white font-semibold", router.pathname === "/log-in" ? "text-4xl" : "text-2xl")}><Link href={"/"}>tweety</Link></h1>
+
+            <h1 className={cls("text-[#4286f4] font-title font-semibold", router.pathname === "/log-in" ? "hidden" : "text-2xl")}><Link href={"/"}>tweety</Link></h1>
         </div>
     );
 }
--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title(type: { nickname: string }) {
     return (
         <div className='py-12'>
-            <span className='text-white text-4xl font-semibold px-9'>{`Hello,  ${type.nickname}!`}</span>
+            <span className='text-white drop-shadow-md text-5xl font-semibold px-9'>{`Hello,  ${type.nickname}!`}</span>
         </div>
     );
 }
