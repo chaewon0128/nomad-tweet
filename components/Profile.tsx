7f7a1ad3a504bbdd545558342a3ff03b572d4a46
chaewon <juh7814@gmail.com>
2023-07-21 13:48:28 +0900

    ⚡️ 프로필 데이터 수정

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,18 +1,14 @@
+import { ProfileType } from "../type/type";
 
 
-type ProfileType = {
-    nicname: string,
-    id?: string,
-    imageUrl?: string,
-}
 
-export default function Profile({ nicname, id, imageUrl }: ProfileType) {
+export default function Profile({ name, email, avatarUrl }: ProfileType) {
     return (
         <div className="flex space-x-3 items-start">
             <div className='bg-red-300 rounded-full w-14 h-14 shadow-md' />
             <div className='w-[80%] flex flex-col'>
-                <span className='text-xl font-semibold inline-block mr-2'>{nicname}</span>
-                <span className="text-sm inline-block">{`@${id}`}</span>
+                <span className='text-xl font-semibold inline-block mr-2'>{name}</span>
+                <span className="text-sm inline-block">{`@${email?.split("@")[0]}`}</span>
             </div>
 
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,40 @@
-import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
 import TweetMsg from '../components/TweetMessage';
-import Input from '../components/Input';
+import TweetInput from '../components/TweetInput';
 import Title from '../components/Title';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import useMutation from '../lib/useMutation';
 
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 export default function Home() {
+  const router = useRouter()
+  const { data, error } = useSWR("/api/profile", fetcher)
+  const { data: tweet } = useSWR("/api/post", fetcher)
+  const [mutation, { data: posting, loading, error: postingError }] = useMutation("/api/post")
+
+  const onTweet = () => {
+    mutation(data)
+
+  }
+  useEffect(() => {
+    if (error) {
+      router.replace("/log-in")
+    }
+
+  }, [error])
+
 
   return (
-    <div className='pt-10 bg-gradient-to-r from-[#373B44] to-[#4286f4]'>
-      <Title nickname='nico' />
-      <div className='bg-white min-h-full rounded-t-3xl py-3 px-7'>
+    <div className='pt-10 bg-gradient-to-br'>
+      <Title nickname={data?.profile.name} />
+      <div className='bg-white min-h-screen rounded-t-3xl py-3 px-7'>
         {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <TweetMsg index={i} />
+          <TweetMsg index={i} key={i} profile={data?.profile} />
         ))}
       </div>
-      <Input />
+      <TweetInput />
     </div>
   );
 }
