854c919bc233dcdc6ec2a24776933f1e405171d4
chaewon <juh7814@gmail.com>
2023-07-22 15:40:18 +0900

    💄 네비게이션 height, width 수정

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,8 +8,7 @@ export default function Nav() {
     const router = useRouter()
 
     return (
-        <div className="absolute top-0 p-4" >
-
+        <div className="absolute top-0 p-4 h-16  w-full" >
             <h1 className={cls("text-[#4286f4] font-title font-semibold", router.pathname === "/log-in" ? "hidden" : "text-2xl")}><Link href={"/"}>tweety</Link></h1>
         </div>
     );
