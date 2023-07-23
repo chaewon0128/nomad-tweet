befd8c12d03defc2b6f032ff5c513e5e9018381e
chaewon <juh7814@gmail.com>
2023-07-24 04:33:22 +0900

    🐛  create account api 수정

--- pages/api/create-account.ts
+++ pages/api/create-account.ts
@@ -15,21 +15,20 @@ export default async function handler(req: NextApiRequest,
                 email,
             }
         })
-        if (user) {
-            return res.status(400).json({
-                message: "이미 존재하는 아이디 입니다"
-            })
-        } else {
+        if (!user) {
             await db.user.create({
                 data: {
                     name,
                     email,
                     password: encryptPassword,
-
                 }
             })
             return res.json({
                 status: 201,
+            })
+        } else {
+            return res.status(400).json({
+                message: "이미 존재하는 아이디 입니다"
             })
         }
 
