c80414076b020ac69ed61226e0c396ccc097453e
chaewon <juh7814@gmail.com>
2023-07-23 19:11:43 +0900

    🍱 프로필 이미지 수정

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,4 +1,5 @@
-
+import Image from "next/image";
+import profileImg from '../image/profile.png'
 import { PreviewType } from '../type/type';
 
 export default function Preview({ register, avatarPreview }: PreviewType) {
@@ -6,7 +7,8 @@ export default function Preview({ register, avatarPreview }: PreviewType) {
         <>
             <label htmlFor="avatar" className="cursor-pointer" >
                 <div className="flex justify-center items-center flex-col space-y-2  px-10 py-5">
-                    <div className='bg-red-300 rounded-full w-16 h-16 shadow-md relative text-center' >
+                    <div className='bg-yellow-200  rounded-full w-16 h-16 shadow-md relative text-center' >
+                        <Image src={profileImg} alt="profile" width={300} height={300} />
                         {avatarPreview ? <img src={avatarPreview} className="rounded-full w-16 h-16 absolute" /> : null}
                         <div className="text-xs flex items-center rounded-full w-16 h-16 opacity-0 text-white duration-300 bg-opacity-30 bg-slate-500  hover:opacity-100 z-20 hover:transition-all">Avatar Change</div>
                     </div>
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,12 +1,15 @@
 import emailToId from "../lib/emailToId";
 import { ProfileType } from "../type/type";
-
+import Image from "next/image";
+import profileImg from '../image/profile.png'
 
 
 export default function Profile({ name, email, avatarUrl }: ProfileType) {
     return (
         <div className="flex space-x-3 items-start">
-            <div className='bg-yellow-200 rounded-full w-14 h-14 shadow-md' />
+            <div className='bg-yellow-200 rounded-full w-14 h-14 shadow-md'>
+                <Image src={profileImg} alt="profile" width={300} height={300} />
+            </div>
             <div className='w-[80%] flex flex-col'>
                 <span className='text-xl font-semibold inline-block mr-2'>{name}</span>
                 <span className="text-sm inline-block">{emailToId(email)}</span>
