3cd3e6264fd8655932644dbccf0eb084c57529c4
chaewon <juh7814@gmail.com>
2023-07-24 16:19:36 +0900

    🐛  heroicons 안 보이는 이슈 수정

--- components/button/XButton.tsx
+++ components/button/XButton.tsx
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import { XmarkIcon } from "../../icons/icons";
 import { XButtonType } from "../../type/type";
 
 
@@ -13,7 +12,9 @@ export default function XButton({ page, position }: XButtonType) {
     }
     return (
         <button className={`absolute right-6 ${position}`} onClick={page === "back" ? onBackPage : onClosePage}>
-            <XmarkIcon size="8" />
+            <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+            </svg>
         </button>
     );
 }
