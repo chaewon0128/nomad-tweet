1a202dd9a7dd403ffffbcdc32eae479a42d8b48c
chaewon <juh7814@gmail.com>
2023-08-03 18:47:38 +0900

    🍱  회원가입 페이지 이미지 수정

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,25 +1,24 @@
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Input from "../components/Input";
 import { FormValue } from "../type/type";
 import MainBtn from "../components/button/MainBtn";
-import Preview from "../components/Preview";
 import XButton from "../components/button/XButton";
 import useMutation from "../lib/useMutation";
 import { Toaster, toast } from "react-hot-toast";
 import { useRouter } from "next/router";
-
+import tweety from '../image/create.png'
+import Image from "next/image";
 
 
 export default function Create() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<FormValue>({ mode: 'onBlur' })
+    const { register, handleSubmit, formState: { errors } } = useForm<FormValue>({ mode: 'onBlur' })
     const [mutation, { loading, data }] = useMutation("/api/create-account")
-    const [avatarPreview, setAvatarPreview] = useState("")
-    const avatar = watch("avatar")
     const router = useRouter()
     const onSignUp = async (validForm: FormValue) => {
         if (loading) return;
-        await mutation(validForm)
+        mutation(validForm)
+
     }
     useEffect(() => {
         if (data?.status === 201) {
@@ -32,27 +31,20 @@ export default function Create() {
     }, [data])
 
 
-    useEffect(() => {
-        if (avatar && avatar.length > 0) {
-            setAvatarPreview(URL.createObjectURL(avatar[0]))
-        }
-
-
-    }, [avatar])
-
-
     return (
         <div className='pt-20 bg-gradient-to-br'>
-            < div className='bg-white w-full  animatecss animatecss-fadeInUp rounded-t-3xl flex flex-col space-y-12 items-center justify-center h-[683px] relative' >
+            < div className='bg-white w-full  animatecss animatecss-fadeInUp rounded-t-3xl flex flex-col space-y-1 items-center justify-center h-[683px] relative' >
                 <h2 className="font-extrabold text-5xl mt-16 text-[#4286f4]">Create an account</h2>
+                <div className="w-32  m-auto">
+                    <Image src={tweety} alt="tweety" />
+                </div>
                 <form onSubmit={handleSubmit(onSignUp)} className="w-[90%] flex flex-col space-y-2">
-                    <Preview register={register} avatarPreview={avatarPreview} />
                     <Input title="name" type="text" register={register} formName="name" errors={errors} />
                     <Input title="email" type="email" register={register} formName="email" errors={errors} />
                     <Input title="password" type="password" register={register} formName="password" errors={errors} required={true} />
                     <MainBtn title="Sign Up" loading={loading} />
                 </form>
-                <XButton page="/log-in" position="top-[-20px]" />
+                <XButton page="/log-in" position="top-[20px]" />
             </div >
             <div><Toaster /></div>
         </div >
