c17843979980b0a95943644d0a31cc33f9c3b767
chaewon <juh7814@gmail.com>
2023-08-14 15:59:51 +0900

    💄 메인페이지 UI 수정

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -16,7 +16,7 @@ export default function Textarea() {
     return (
         <form className="relative pb-10 border-b" onSubmit={handleSubmit(onAnswer)} >
             <textarea {...register("answer", { required: true })} rows={3} placeholder="Tweet your reply" className=" w-full mt-5 outline-none resize-none" />
-            <button className="bg-[#4286f4] absolute bottom-3 right-0 rounded-full px-4 py-2 text-white">tweet</button>
+            <button className="bg-[#4286f4] absolute bottom-3 right-0 rounded-full px-4 py-2 text-white hover:bg-blue-600">tweet</button>
         </form>
     );
 }
--- components/TweetMessage.tsx
+++ components/TweetMessage.tsx
@@ -5,15 +5,22 @@ import dateInvert from '../lib/dateInvert';
 
 
 
-export default function TweetMsg({ index, profile, content, date, name, email }: TweetMsgType) {
+export default function TweetMsg({ index, profile, content, date, liked, answer }: TweetMsgType) {
+
 
     return (
-        <div className='border-b mt-6 pb-4 last:border-b-0'>
+        <div className='border-b my-3  p-6 rounded-3xl last:border-b-0 relative hover:bg-yellow-100'>
             <Link href={`/tweet/${index}`}>
                 <div className='space-x-16 cursor-pointer'>
-                    <Profile name={name} email={email} avatarUrl={profile?.avatarUrl} />
-                    <p className='mt'>{content}</p>
-                    <div className='text-right text-sm mt'>{dateInvert(date)}</div>
+                    <Profile name={profile?.name} email={profile?.email} avatarUrl={profile?.avatarUrl} />
+                    <p >{content}</p>
+                    <div className='text-xs top-4 right-4 absolute '>{dateInvert(date)}</div>
+                    <div className='text-right text-xs'>
+                        <span>
+                            {`💗 ${liked}`}
+                        </span>
+                        <span className='ml-3'>{`💬 ${answer}`}</span>
+                    </div>
                 </div>
             </Link>
         </div>
