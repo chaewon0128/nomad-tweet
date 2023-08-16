673c9432c313a39df79e1ca422c5041710a542c5
chaewon <juh7814@gmail.com>
2023-08-16 16:46:38 +0900

    🐛  document 파일 함수형 타입으로 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
-
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Ysabeau+Infant:ital,wght@1,900&display=swap" rel="stylesheet" />
-                    <title>Tweety</title>
-                </Head>
-                <body className='min-h-screen border m-auto from-[#fffc00] to-[#ffffff] w-[500px]'>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Ysabeau+Infant:ital,wght@1,900&display=swap" rel="stylesheet" />
+                <title>Tweety</title>
+            </Head>
+            <body className='min-h-screen border m-auto from-[#fffc00] to-[#ffffff] w-[500px]'>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
 
-export default MyDocument;
