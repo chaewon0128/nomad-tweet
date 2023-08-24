01dfaafdc8cbd8b838987c1e8e3ffcd8de74218c
chaewon <juh7814@gmail.com>
2023-08-24 18:49:32 +0900

    🐛  초기로딩시 타이틀에 undefind 되는 오류 수정

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,9 +1,9 @@
 
 
-export default function Title(type: { nickname: string, isLoading: boolean }) {
+export default function Title(type: { nickname: string }) {
     return (
         <div className='py-12'>
-            <span className='text-white drop-shadow-md text-5xl font-semibold px-9'>Hello, {type.isLoading ? <div className="spinner text-lg"></div> : `${type.nickname}!`}</span>
+            <span className='text-white drop-shadow-md text-5xl font-semibold px-9'> {type.nickname ? `Hello, ${type.nickname}!` : ""}</span>
         </div>
     );
 }
